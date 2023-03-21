0b4690640bd2e3500883d9a59d80e2fab7109528
Ragavan2020 <vijayaragavans2020@gmail.com>
2023-03-21 12:00:17 +0530

    Java Basics

---
+++ Hello_World.java
@@ -0,0 +1,15 @@
+package Basic_Programs;
+
+public class Hello_World { // first we have to create Class name
+
+	public static void main(String[] args) { //main method (method-set of instruction)
+		
+		
+		
+		System.out.println("Hello world 1");  //that method (set of instruction) already  written in java 
+	
+	//	System.out.print("Hello ");
+	//	System.out.print("World 2 ");
+	}
+
+}
